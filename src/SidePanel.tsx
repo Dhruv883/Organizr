ff95feb584e39ce30e732d135d761ce296cea627
Dhruv Dedhia <dhruvdedhia03@gmail.com>
2024-12-12 13:22:37 +0530

    Initial commit

---
+++ src/SidePanel.tsx
@@ -0,0 +1,9 @@
+function SidePanel() {
+  return (
+    <div className="w-screen h-screen border-2 border-red-500 rounded-2xl p-1">
+      SidePanel Hopefully
+    </div>
+  );
+}
+
+export default SidePanel;
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./manifest.json";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), crx({ manifest })],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, 'index.html')
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+});
