840ccaaaeee289f6d4ed3b0282dc11c6eed7ba23
Dhruv Dedhia <dhruvdedhia03@gmail.com>
2024-12-19 20:49:41 +0530

    Fetch Bookmarks

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import SidePanel from "./SidePanel.tsx";
+import SidePanel from "./SidePanel.jsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
